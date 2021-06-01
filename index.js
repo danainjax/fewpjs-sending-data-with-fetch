58e0ec3f03bbe37748a0d333798a8bc0e61d1602
danainjax <danainjax@gmail.com>
2021-06-01 13:32:28 -0400

    coded along

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+// fetch("http://localhost:3000/dogs", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Amelia",
+//         dogBreed: "miniature pin"
+//     })
+// }
+// );
+
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+            })
+        })
+
+    .then( function (response) {
+        return response.json()
+        
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+        console.log(object["id"])
+      })
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+}
+
+
